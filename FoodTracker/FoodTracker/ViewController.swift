8bb89903f2237f4fb680009fd5ee512737d56e99
shaynavins <51338693+shaynavins@users.noreply.github.com>
2019-06-03 15:13:07 -0400

    foodtracker

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by vinoth rajasekaran on 16/12/17.
+//  Copyright © 2017 none. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //MARK: PROPERTIES
+    @IBOutlet weak var nameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBOutlet weak var hoIAmShaynaLabel: UILabel!
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+
+    
+}
+
